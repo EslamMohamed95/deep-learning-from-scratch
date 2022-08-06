8e0630cd937400ea4a5c02703a3eca7f30194e5a
EslamMohamed95 <islam_hikal@hotmail.com>
2022-08-06 19:43:52 +0200

    initial commit

--- neural_network_backend.py
+++ neural_network_backend.py
@@ -16,7 +16,7 @@ class NeuralNetwork(object):
                                                          (self.hidden_nodes, self.output_nodes))
         self.lr = learning_rate
 
-        self.activation_function = lambda x: 1.0 / (1.0 + np.exp(-x))
+        self.activation_function = lambda x: 1.0 / (1.0 + np.exp(-x))  # sigmoid function
 
     def train(self, features, targets):
         ''' Train the network on batch of features and targets. 
@@ -96,8 +96,10 @@ class NeuralNetwork(object):
             n_records: number of records
 
         """
-        self.weights_hidden_to_output += self.lr*delta_weights_h_o/n_records  # update hidden-to-output weights with gradient descent step
-        self.weights_input_to_hidden += self.lr*delta_weights_i_h/n_records  # update input-to-hidden weights with gradient descent step
+        self.weights_hidden_to_output += self.lr * delta_weights_h_o / n_records  # update hidden-to-output weights
+        # with gradient descent step
+        self.weights_input_to_hidden += self.lr * delta_weights_i_h / n_records  # update input-to-hidden weights
+        # with gradient descent step
 
     def run(self, features):
         """ Run a forward pass through the network with input features
